8cf50bd30beebd7ccbedbf44efaa503eb53e2f76
Elena Tsvetkova <kaello.skye@gmail.com>
2018-10-11 17:11:17 +0100

    Remove hardcoded data, add comments

--- owl_bot.py
+++ owl_bot.py
@@ -1,33 +1,49 @@
 #!/usr/bin/env python
-
+import os
+import json
 import telebot
 from random import choice
 
-# токен и прокси для запуска на локальной машине
-TGBOT_TOKEN = '695541521:AAGv5DL2dt4GYuaD1qU2JKVdNMJ9nCtBT8E'
-TGBOT_PROXY = 'socks5://tg-kaelloskye:VACey4bI@ru.socksy.seriyps.ru:7777'
 
-# если прокси указан - использовать
-if TGBOT_PROXY:
-    telebot.apihelper.proxy = dict(https=TGBOT_PROXY)
+def load_config(filename):
+    """ Возвращает словарь с настройками
 
-bot = telebot.TeleBot(TGBOT_TOKEN)
+    Настройки прокси, файл с текстом приветствия, директория для
+    сохранения временных файлов)
+    """
+    with open(filename, encoding='utf-8') as data:
+        config = json.load(data)
+
+    config['token'] = os.environ['TGBOT_TOKEN']
+    config['proxy'] = dict(https=config['proxy']) if config['proxy'] else None
+    return config
+
+
+# загрузка конфигурации и прокси (если указаны)
+config = load_config('config.json')
+telebot.apihelper.proxy = config['proxy']
+
+bot = telebot.TeleBot(config['token'])
 
 
 @bot.message_handler(content_types=['sticker'])
 def get_sticker(message):
+    """ Возвращает пользователю отправленный им стикер в виде фото """
     bot.send_message(message.chat.id, 'Вы прислали стикер!')
 
 
 @bot.message_handler(regexp=r'подтвер[ж]?д[аи]+[е]?(?:шь)?')
 def confirm_smth(message):
-    words = ['Подтверждаю', 'Угу']
-    bot.send_message(message.chat.id, choice(words))
+    """ Отправляет пользователю сообщение, если пользователь попросил
+    что-либо подтвердить """
+    reply = choice(config['bot_replies'])
+    bot.send_message(message.chat.id, reply)
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome_msg(message):
-    with open('joke.txt', encoding='utf-8') as f:
+    """ Приветственное сообщение """
+    with open(config['start_msg_file'], encoding='utf-8') as f:
         welcome_msg = f.read()
     bot.send_message(message.chat.id, welcome_msg)
 
