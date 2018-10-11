a2658544e91e41cd088f50954c4a5b09a32284c5
Elena Tsvetkova <kaello.skye@gmail.com>
2018-10-11 10:08:34 +0100

    Bot greets user on command /start

---
+++ owl_bot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import telebot
+
+# токен и прокси для запуска на локальной машине
+TGBOT_TOKEN = '695541521:AAGv5DL2dt4GYuaD1qU2JKVdNMJ9nCtBT8E'
+TGBOT_PROXY = 'socks5://tg-kaelloskye:VACey4bI@ru.socksy.seriyps.ru:7777'
+
+# если прокси указан - использовать
+if TGBOT_PROXY:
+    telebot.apihelper.proxy = dict(https=TGBOT_PROXY)
+
+bot = telebot.TeleBot(TGBOT_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome_msg(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+
+bot.polling()
